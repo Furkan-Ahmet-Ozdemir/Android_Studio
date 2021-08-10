0a2383a772fb84c1644f56586e95d1f393f2c3ae
Furkan-Ahmet-Ozdemir <furkanahmetozdemir@hotmail.com>
2021-08-10 15:48:08 +0300

    Initial commit

--- AS_10_ProgB_ToggleB/app/src/main/java/com/opcd/as_10_progb_toggleb/MainActivity.java
+++ AS_10_ProgB_ToggleB/app/src/main/java/com/opcd/as_10_progb_toggleb/MainActivity.java
@@ -6,12 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import java.sql.SQLOutput;
 
 public class MainActivity extends AppCompatActivity {
 
     private ToggleButton toggleButton;
     private ImageView imageView;
+    private SeekBar seekBar;
+    private TextView txtSeekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         toggleButton = (ToggleButton)findViewById(R.id.toggle);
         imageView = (ImageView)findViewById(R.id.image23);
+        seekBar = (SeekBar)findViewById(R.id.seekBar2);
+        txtSeekBar = (TextView)findViewById(R.id.textSeek);
 
         toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -29,7 +37,22 @@ public class MainActivity extends AppCompatActivity {
                     imageView.setVisibility(View.INVISIBLE);
             }
         });
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                txtSeekBar.setText(String.valueOf(i));
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                System.out.println("Seek Bar haraket edebilir.");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                System.out.println("Seek Bar durdu.");
+            }
+        });
 
     }
 }
